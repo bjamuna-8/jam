0bba766a4a3bc815a91df83fa739818a9688c555
root <root@ip-172-31-7-110.ap-south-1.compute.internal>
2024-12-26 07:29:54 +0000

    Second Version of Java Code

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,10 @@ class Main
 public static void main(String[] args)
 {
 System.out.println("Hello World");
+System.out.println("Hello  Jamuna");
+int a=10,b=20;
+int c=a+b;
+System.out.println(c);
 }
 }
  
